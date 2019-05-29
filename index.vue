68802e5784408d5356e3529e1e37eed56d094c15
yangzhenzhen123 <50770451+yangzhenzhen123@users.noreply.github.com>
2019-05-29 23:42:15 +0800

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,37 @@
+ <template>
+        <div>
+          <input type="text" v-model="text">
+          <button @click="handClick">提交<button>
+          <ul class="todo-list">
+            <li
+            v-for="(item, index) in list"
+            :key="index"
+            @click="removeItem(index)"
+            >
+              {{item}}
+            <li>
+          </ul>
+        </div>
+      </template>
+      
+      <script>
+        export default {
+          data() {
+            return({
+              list: [
+                '每天都有个好心情',
+              ],
+              text: ''
+            })
+          },
+          methods: {
+            handClick() {
+              this.list.push(this.text)
+              this.text = ''
+            },
+            removeItem(index){
+              this.list.splice(index, 1)
+            }
+          }
+        }
+      </script> 
